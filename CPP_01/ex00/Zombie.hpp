24aa0be64b7eb4f3324c7fb15d3f73bf84ad21fd
Anatolii <117525743+AshParker19@users.noreply.github.com>
2023-12-24 23:00:53 +0000

    ex00 fixed tests

--- CPP_01/ex00/Zombie.hpp
+++ CPP_01/ex00/Zombie.hpp
@@ -9,7 +9,7 @@ class Zombie
         std::string name;
     
     public:
-        Zombie(std::string new_name) : name(new_name) {}
+        Zombie(std::string new_name) : name(new_name) {} // TODO: know if can we have initializing list in a header
         ~Zombie();
 
         void announce( void );
--- CPP_01/ex00/main.cpp
+++ CPP_01/ex00/main.cpp
@@ -20,15 +20,22 @@ int main()
 {
     // test a dynamically allocated object in the scope of the same fucntion
     Zombie *zombie_guy = newZombie("guy"); 
-    zombie_guy->announce();
-    delete(zombie_guy);
-
+    if (zombie_guy) // if memory allocation failed, exception will be thrown and NULL will be returned
+    {
+        zombie_guy->announce();
+        // zombie_guy->~Zombie(); // if we just call a destructor, memory won't be freed
+        delete zombie_guy; // prove that 'delete' calls a destructor
+    }
+        
     std::cout << std::endl;
 
     // test a dynamically allocated object in the other function which returns a pointer to it
     Zombie *zombie_bro = return_heap();
-    zombie_bro->announce();
-    delete zombie_bro;
+    if (zombie_bro)
+    {
+        zombie_bro->announce();
+        delete zombie_bro;
+    }
 
     std::cout << std::endl;
 
