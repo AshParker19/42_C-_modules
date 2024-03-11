85630588a543cb4a2d3bd037b63718204737b652
AshParker19 <ashparker4219@gmail.com>
2024-03-11 17:53:02 +0000

    ex00 done

--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -2,13 +2,15 @@
 
 int main(int ac, char **av)
 {
-    if (ac != 2)
+    if (ac != 3)
     {
         std::cout << "Wrong number of arguments!" << std::endl;
         return (1);
     }
 
+    int value;
     {
+        value = atoi(av[1]);
         std::vector<int> testVector (5);
         testVector.push_back(10);
         testVector.push_back(20);
@@ -16,8 +18,20 @@ int main(int ac, char **av)
         testVector.push_back(40);
         testVector.push_back(50);
 
-        int value;
-        value =  easyfind(testVector, std::atoi(av[1]));
+        value =  easyfind(testVector, value);
+        std::cout << "The value --> " << value << std::endl;
+    }
+    std::cout << "==================================" << std::endl;
+    {
+        value = atoi(av[2]);
+        std::list<int> testList;
+        testList.push_back(11);
+        testList.push_front(22);
+        testList.push_back(33);
+        testList.push_front(44);
+        testList.push_back(55);
+
+        value = easyfind(testList, value);
         std::cout << "The value --> " << value << std::endl;
     }
 }
