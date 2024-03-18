8ab0c38a7b0a782f8f618760744d3bc5635bd206
AshParker19 <ashparker4219@gmail.com>
2024-03-18 19:45:26 +0000

    ex00 now catches exceptions

--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -8,30 +8,38 @@ int main(int ac, char **av)
         return (1);
     }
 
-    int value;
+    try 
     {
-        value = atoi(av[1]);
-        std::vector<int> testVector (5);
-        testVector.push_back(10);
-        testVector.push_back(20);
-        testVector.push_back(30);
-        testVector.push_back(40);
-        testVector.push_back(50);
+        int value;
+        {
+            value = atoi(av[1]);
+            std::vector<int> testVector (5);
+            testVector.push_back(10);
+            testVector.push_back(20);
+            testVector.push_back(30);
+            testVector.push_back(40);
+            testVector.push_back(50);
 
-        value =  easyfind(testVector, value);
-        std::cout << "The value --> " << value << std::endl;
+            value =  easyfind(testVector, value);
+            std::cout << "The value --> " << value << std::endl;
+        }
+        std::cout << "==================================" << std::endl;
+        {
+            value = atoi(av[2]);
+            std::list<int> testList;
+            testList.push_back(11);
+            testList.push_front(22);
+            testList.push_back(33);
+            testList.push_front(44);
+            testList.push_back(55);
+
+            value = easyfind(testList, value);
+            std::cout << "The value --> " << value << std::endl;
+        }
     }
-    std::cout << "==================================" << std::endl;
+    catch(const std::exception &e)
     {
-        value = atoi(av[2]);
-        std::list<int> testList;
-        testList.push_back(11);
-        testList.push_front(22);
-        testList.push_back(33);
-        testList.push_front(44);
-        testList.push_back(55);
-
-        value = easyfind(testList, value);
-        std::cout << "The value --> " << value << std::endl;
+        std::cerr << "Error: " << e.what() << std::endl;
+        return (1);
     }
 }
